2e945f122cd85a9ba781a6662a972ba7c6be9cdc
eferlius <ferlinghetti.enrico@gmail.com>
2023-12-04 13:53:40 +0100

    function copy past with write on frames

--- functions_copy_paste.py
+++ functions_copy_paste.py
@@ -55,6 +55,49 @@ def function_copy_paste_save_video_with_operations_on_frames(video_source_path_o
     cap.release()
     videoWriter.release()
 
+def function_copy_paste_save_video_with_operations_on_frames_write_on_frames(video_source_path_or_cap, video_dest_path, fps = -1, 
+                                    OTHER_PARAMETERS_FOR_YOUR_FUNCTION = None,
+                                    timestamp_list = None,
+                                    string_fmt = 'frame: {:05d} time: {:03.3f}',
+                                    origin = (20, 20),
+                                    font = cv2.FONT_HERSHEY_SIMPLEX,
+                                    fontScale = 1, 
+                                    color = (0, 0, 255),
+                                    thickness = 1):
+    cap = video_lib.general.get_cap(video_source_path_or_cap)
+    total_frames = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+    if fps == -1:
+        fps = cap.get(cv2.CAP_PROP_FPS)
+    video_writer_initialized = False
+    for i in range(total_frames):
+        ret, frame = cap.read()
+        if ret == True:
+            if not video_writer_initialized:
+                    h, w = frame.shape[:2]
+                    # fourcc = cv2.VideoWriter_fourcc('X','V','I','D')
+                    fourcc = cv2.VideoWriter_fourcc('M','J','P','G')
+                    videoWriter = cv2.VideoWriter(video_dest_path, fourcc, fps, (w,h))
+                    video_writer_initialized = True
+            try:
+                # write here all the operations on the image
+                # result = foo(frame)
+                imgForVideo = frame # cancel this line          
+                if not timestamp_list is None:
+                    timestamp = timestamp_list[i]
+                else:
+                    timestamp = i / fps 
+                stringForImage =  string_fmt.format(i, timestamp)
+                imgForVideo = cv2.putText(result, stringForImage, origin, font, fontScale, color, thickness, cv2.LINE_AA)
+                videoWriter.write(imgForVideo)
+            except:
+                print('error occurred in the loop at iteration {}'.format(i))
+                pass
+        else:
+            print('not recognized frame at iteration {}'.format(i))
+            break
+    cap.release()
+    videoWriter.release()
+
 def function_copy_paste_play_video_with_operations_on_frames(video_source_path_or_cap, 
                                                              video_dest_path, fps = -1, 
                                                              OTHER_PARAMETERS_FOR_YOUR_FUNCTION = None):
